7e0ec7206cd55ef81bad049995b6d0acf0e0d6b3
Adem Hatay <hatayadem5@gmail.com>
2023-10-08 16:56:43 +0300

    BoxDecoration

--- lib/main.dart
+++ lib/main.dart
@@ -18,17 +18,31 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-              width: 100,
-              height: 100,
-              color: Colors.red,
-              alignment: Alignment.center,
-              child: Container(
-                width: 50,
-                height: 50,
-                color: Colors.blue,
-                alignment: Alignment.center,
-                child: const Text('Hello'),
-              )),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+              border: Border.all(
+                color: Colors.red,
+                width: 3,
+              ),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1695653422718-48c2cc37caf7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: const Text(
+              "I'm",
+              style: TextStyle(
+                fontSize: 70,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
